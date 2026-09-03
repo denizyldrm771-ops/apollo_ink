7a95058b6f5b7c17f77389063b494e59e85cb7bf
denizyldrm771-ops <denizyldrm771@gmail.com>
2026-09-03 19:50:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,5 @@
-const CACHE_NAME = 'apollo-ink-v5';
-
-const ASSETS = [
-  './',
-  './index.html',
-  './icon.png',
-  './manifest.webmanifest',
-  './sun-231E1A.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
+const CACHE='apollo-ink-v5';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png','./sun.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
